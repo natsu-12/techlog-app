e3be20d0d20319aebdf6a2bf4badf5de572024f5
kazuya <kazunatsu1218@gmail.com>
2025-08-20 16:52:14 +0900

    学習ログ削除機能を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,24 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
-  
+
   def index
-    @posts = Post.limit(10).order(created_at: :desc)
+    @posts = Post.limit(10).order(created_at: :desc) # 作成日時が新しい順に10件
   end
 
   def new
-    @post = Post.new 
+    @post = Post.new # 新規投稿用のインスタンス変数を用意
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-
+    @post = Post.new(post_params) # ストロングパラメータを使ってフォームから受け取ったパラメータを許可
+    @post.user_id = current_user.id # ログインユーザのIDを代入して関連付け
 
     if @post.save
-      flash[:notice] = '投稿しました' 
+      flash[:notice] = '投稿しました' # 成功時のフラッシュメッセージ
       redirect_to posts_path
     else
-      flash[:alert] = '投稿に失敗しました' 
-      render :new 
+      flash[:alert] = '投稿に失敗しました' # 失敗時のフラッシュメッセージ
+      render :new # 投稿画面を再表示
     end
   end
 
@@ -28,10 +27,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    post = Post.find_by(id: params[:id])
+
+    if post.user == current_user
+      post.destroy
+      flash[:notice] = '投稿が削除されました'
+    end
+    redirect_to posts_path
   end
-   
+
   private
- # Strong parameters for post
+
+  # ストロングパラメータで許可するカラムを指定
   def post_params
     params.require(:post).permit(:title, :content) # title と content のみ許可
   end
